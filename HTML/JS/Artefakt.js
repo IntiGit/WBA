42f44ff57da2da17bfe7edefff7b9f94bc8181d8
Oliver Adler <oliver_elias.adler@hsbi.de>
2024-06-03 21:02:27 +0200

    html angepasst und artefakt hinzugefügt.

--- HTML/JS/Artefakt.js
+++ HTML/JS/Artefakt.js
@@ -1,5 +1,4 @@
-
-class Artefakt {
+ class Artefakt {
     constructor(id, titel, kurzBeschreibung, aufgabenbereich, dauer) {
         if (kurzBeschreibung.length > 255) {
             throw new Error('Kurzbeschreibung darf maximal 255 Zeichen lang sein');
